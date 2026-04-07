60f61dd71d0d15b1f2334cfd7127e302e9d05bc5
Bharath Kumar Boya <bharathkumar219219@gmail.com>
2026-04-07 10:22:30 +0530

    Update and rename Employees with their salaries to Employees with their salaries.sql

--- Employees with their salaries.sql
+++ Employees with their salaries.sql
@@ -1,4 +1,5 @@
-Q. We have a table with employees and their salaries, however, some of the records are old and contain outdated salary information. 
+/* 
+We have a table with employees and their salaries, however, some of the records are old and contain outdated salary information. 
 Find the current salary of each employee assuming that salaries increase each year. Output their id, first name, last name, department ID, and 
 current salary. Order your list by employee ID in ascending order.
 
@@ -10,6 +11,7 @@ salary:bigint
 department_id:bigint
 
 SOLUTION:
+*/
 
 SELECT
     id,
