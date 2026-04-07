2d2a874bffb92d1a727f2a71d07041fd8c71e316
Bharath Kumar Boya <bharathkumar219219@gmail.com>
2026-04-07 10:27:26 +0530

    Update and rename Find the top 5 states with the most 5 star businesses to Find the top 5 states with the most 5 star businesses.sql

--- Find the top 5 states with the most 5 star businesses.sql
+++ Find the top 5 states with the most 5 star businesses.sql
@@ -1,4 +1,5 @@
-Q. Find the top 5 states with the most 5 star businesses. Output the state name along with the number of 5-star businesses and order 
+/*
+Find the top 5 states with the most 5 star businesses. Output the state name along with the number of 5-star businesses and order 
 records by the number of 5-star businesses in descending order. In case there are ties in the number of businesses, return all the unique 
 states. If two states have the same result, sort them in alphabetical order.
 
@@ -16,7 +17,7 @@ stars:double
 review_count:bigin
 tis_open:bigint
 categories:text
-
+*/
 
 SELECT 
     state,
