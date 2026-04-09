51472d0dc0fcd337b9d8078886c323394b21f09b
Bharath Kumar Boya <bharathkumar219219@gmail.com>
2026-04-09 11:01:16 +0530

    Update and rename Percentage of loan's balance to the total balance to Percentage of loan's balance to the total balance.sql

--- Percentage of loan's balance to the total balance.sql
+++ Percentage of loan's balance to the total balance.sql
@@ -1,5 +1,5 @@
-
-Q. Write a query that returns the rate_type, loan_id, loan balance , and a column that shows with what percentage the loan's balance contributes to the total balance among the loans of the same rate type. Sort the final output by rate_type and loan_id.
+/*
+Write a query that returns the rate_type, loan_id, loan balance , and a column that shows with what percentage the loan's balance contributes to the total balance among the loans of the same rate type. Sort the final output by rate_type and loan_id.
 Table: submissions
 balance:double precision
 id:bigint
@@ -8,6 +8,7 @@ loan_id:bigint
 rate_type:text
 
 SOLUTION:
+*/
 
 SELECT
   rate_type,
