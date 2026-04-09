3cfafd27f4972196b1857c29f46a158d57afd877
Bharath Kumar Boya <bharathkumar219219@gmail.com>
2026-04-09 11:04:34 +0530

    Update and rename Rising Temperature to Rising Temperature.sql

--- Rising Temperature.sql
+++ Rising Temperature.sql
@@ -1,3 +1,4 @@
+/*
 Table: Weather
 
 +---------------+---------+
@@ -40,7 +41,7 @@ In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
 In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
 
 SOLUTION:
-
+*/
 
 SELECT Id 
 FROM (
