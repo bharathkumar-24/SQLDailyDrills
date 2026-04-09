764dc78c42d829dae26840cf2a818920ad00c50e
Bharath Kumar Boya <bharathkumar219219@gmail.com>
2026-04-09 10:55:52 +0530

    Update and rename Number of unique transactions and total sales to Number of unique transactions and total sales.sql

--- Number of unique transactions and total sales.sql
+++ Number of unique transactions and total sales.sql
@@ -1,4 +1,5 @@
-Q. Find the number of unique transactions and total sales for each of the product categories in 2017. Output the product categories,
+/*
+Find the number of unique transactions and total sales for each of the product categories in 2017. Output the product categories,
 number of transactions, and total sales in descending order. The sales column represents the total cost the customer paid for the product
 so no additional calculations need to be done on the column. Only include product categories that have products sold.
 Tables: wfm_transactions, wfm_products
@@ -16,6 +17,7 @@ product_brand:text
 product_category:text
 product_description:text
 product_id:bigint
+*/
 
 SELECT 
     p.product_category
